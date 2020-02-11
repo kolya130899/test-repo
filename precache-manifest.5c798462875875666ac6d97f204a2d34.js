ee9ab75e4aefccf4587d17e2731996cc4ccff9c0
kolya130899 <kolyawote75@gmail.com>
2020-02-11 14:07:28 +0200

    Updates

---
+++ precache-manifest.5c798462875875666ac6d97f204a2d34.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593d79323053a84c0c2dfa32129e6ca7",
+    "url": "/test-repo/index.html"
+  },
+  {
+    "revision": "3e18d750ea9493c588cd",
+    "url": "/test-repo/static/css/main.f435357c.chunk.css"
+  },
+  {
+    "revision": "fad1f707ff0f80f33de2",
+    "url": "/test-repo/static/js/2.b20fa4a0.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/test-repo/static/js/2.b20fa4a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e18d750ea9493c588cd",
+    "url": "/test-repo/static/js/main.3f2bd297.chunk.js"
+  },
+  {
+    "revision": "8bc81eedf38af28079ed",
+    "url": "/test-repo/static/js/runtime-main.f7d8b364.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/test-repo/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/test-repo/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/test-repo/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "bb5ae98e4ce1a64042093dc235c305ed",
+    "url": "/test-repo/static/media/Raleway-Medium.bb5ae98e.ttf"
+  },
+  {
+    "revision": "9942588a6c84f959132556d99e83ded6",
+    "url": "/test-repo/static/media/Raleway-Regular.9942588a.ttf"
+  },
+  {
+    "revision": "d40214b23a78789d301b000571861df5",
+    "url": "/test-repo/static/media/card-bg1.d40214b2.png"
+  },
+  {
+    "revision": "f2bdae9c148ce45cccab5496ea050f70",
+    "url": "/test-repo/static/media/card-bg2.f2bdae9c.png"
+  },
+  {
+    "revision": "dc29500b12763cb8b6390b28732e4ed8",
+    "url": "/test-repo/static/media/card-bg3.dc29500b.png"
+  },
+  {
+    "revision": "bfee8bc642d6fdc3a94e71cf81948efd",
+    "url": "/test-repo/static/media/open-toggler.bfee8bc6.png"
+  }
+]);
